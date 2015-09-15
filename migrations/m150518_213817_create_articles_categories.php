bbfdaba0b9d75894f0fbbf1aca58329a95da701e
ranvirp <ranvirp@users.noreply.github.com>
2015-09-15 20:22:53 +0530

    Update m150518_213817_create_articles_categories.php

--- migrations/m150518_213817_create_articles_categories.php
+++ migrations/m150518_213817_create_articles_categories.php
@@ -126,32 +126,8 @@ class m150518_213817_articles_table extends Migration
 {
     public function safeUp()
     {
-        /*
-        `name` varchar(255) NOT NULL,
-  `alias` varchar(255) NOT NULL,
-  `description` text,
-  `parentid` int(11) DEFAULT '0',
-  `published` smallint(6) NOT NULL DEFAULT '0',
-  `access` int(11) NOT NULL DEFAULT '0',
-  `language` char(7) NOT NULL,
-  `ordering` int(11) NOT NULL DEFAULT '0',
-  `image` text,
-  `image_caption` varchar(255) DEFAULT NULL,
-  `image_credits` varchar(255) DEFAULT NULL,
-  `params` text,
-  `metadesc` text,
-  `metakey` text,
-  `robots` varchar(20) DEFAULT NULL,
-  `author` varchar(50) DEFAULT NULL,
-  `copyright` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`,`parentid`), 
-  KEY `category` (`published`,`access`), 
-  KEY `parentid` (`parentid`), 
-  KEY `ordering` (`ordering`), 
-  KEY `published` (`published`), 
-  KEY `access` (`access`), 
-  KEY `language` (`language`)
-        */
+      
+        
        $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
@@ -179,6 +155,59 @@ class m150518_213817_articles_table extends Migration
             'copyright'=>Schema::TYPE_STRING.'(50) DEFAULT NULL',
             
                     ], $tableOptions);
+            $this->addPrimaryKey('article_categories_primarykey','{{%atricle_categories}}',['id','parentid']);
+             $this->createTable('{{%article_items}}', [
+            'id' => Schema::TYPE_PK,
+            'alias' => Schema::TYPE_STRING.' NOT NULL',
+            'title'=>Schema::TYPE_STRING.' NOT NULL',
+            'catid'=>Schema::TYPE_INTEGER.'(11) NOT NULL',
+             'userid'=>Schema::TYPE_INTEGER.'(11) NOT NULL',
+             'introtext'=> Schema::TYPE_TEXT,
+             'fulltext'=>Schema::TYPE_TEXT,
+            'name_hi'=>Schema::TYPE_STRING.' NOT NULL',
+            'description'=>Schema::TYPE_TEXT,
+            'parentid'=>Schema::TYPE_INTEGER.' default 0',
+            'published'=>Schema::TYPE_INTEGER.' default 0',
+            'access'=>Schema::TYPE_INTEGER,
+            'language'=>Schema::TYPE_STRING.'(7) NOT NULL',
+            'ordering'=>Schema::TYPE_INTEGER.'(11) NOT NULL DEFAULT 0',
+             'hits'=>Schema::TYPE_INTEGER.'(10) unsigned NOT NULL DEFAULT 0',
+  
+            'created'=>Schema::DATETIME.' NOT NULL',
+            'created_by'=>Schema::TYPE_INTEGER.'(11) NOT NULL DEFAULT 0',
+            'modified'=>Schema::TYPE_DATETIME.'NOT NULL',
+            'modified_by'=>Schema::TYPE_INTEGER.'(11) NOT NULL DEFAULT 0',
+  
+            'image'=>Schema::TYPE_TEXT,
+            'image_caption'=>Schema::STRING.'(255) DEFAULT NULL',
+            'image_credits'=>Schema::STRING.'(255) DEFAULT NULL',
+            'video'=>Schema::TYPE_TEXT,
+            'video_caption'=>Schema::STRING.'(255) DEFAULT NULL',
+            'video_credits'=>Schema::STRING.'(255) DEFAULT NULL',
+             'params'=> Schema::TYPE_TEXT,
+            'metadesc'=>Schema::TYPE_TEXT,
+            'metakey'=>Schema::TYPE_TEXT,
+            'robots'=>Schema::TYPE_STRING.'(20) DEFAULT NULL',
+            'author'=>Schema::TYPE_STRING.'(50) DEFAULT NULL',
+            'copyright'=>Schema::TYPE_STRING.'(50) DEFAULT NULL',
+            
+                    ], $tableOptions);
+            $this->addForeignKey('article_categories_foreignkey','{{%atricle_items}}','catid','{{%atricle_categories}}','id');
+            $this->addForeignKey('article_user_foreignkey','{{%atricle_items}}','userid','{{%user}}','id');
+         $this->createTable('{{%article_attachments}}', [
+            'id' => Schema::TYPE_PK,
+  'id'=>Schema::TYPE_PK,
+  'itemid'=>Schema::TYPE_INTEGER.'(11) NOT NULL',
+  'filename'=> Schema::TYPE_STRING.'(255) NOT NULL',
+  'title'=>Schema::TYPE_STRING.'NOT NULL',
+  'titleAttribute'=>Schema::TYPE_TEXT.' NOT NULL',
+  'hits'=>Schema::TYPE_INTEGER.'(11) NOT NULL',
+  ], $tableOptions);
+     $this->addForeignKey('article_attachment_item_foreignkey','{{%atricle_attachments}}','itemid','{{%atricle_items}}','id');
+         
+ 
+    
+
     }
 
     public function down()
